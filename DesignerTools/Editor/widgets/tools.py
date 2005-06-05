5eb3301d246d5ebe5ff27577df9fa2821a76ffde
mvor <mvor@dbd5520b-6a0a-0410-a553-f54834df5b05>
2005-06-05 18:04:46 +0000

    + ADDED: helper function for launching recoverable error notification message dialogs

--- DesignerTools/Editor/widgets/tools.py
+++ DesignerTools/Editor/widgets/tools.py
@@ -9,3 +9,9 @@ def not_implemented_feature_dialog( window ) :
 		msg_dialog.hide()
 		msg_dialog.destroy()
 
+def recoverable_error( window, message ) :
+	msg_dialog = gtk.MessageDialog( parent=window, type=gtk.MESSAGE_ERROR,buttons=gtk.BUTTONS_CLOSE,
+									message_format = message )
+	msg_dialog.run()
+	msg_dialog.hide()
+	msg_dialog.destroy()
